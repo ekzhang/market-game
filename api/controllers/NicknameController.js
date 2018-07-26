deee61c8adcc1aaf18a5c805b2ba30fa0541e4d8
Eric Zhang <ekzhang1@gmail.com>
2018-07-26 07:57:34 -0700

    Make nicknames more robust

--- api/controllers/NicknameController.js
+++ api/controllers/NicknameController.js
@@ -5,14 +5,19 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const uuid = require('uuid/v4');
+
 module.exports = {
 
   setNick: function(req, res) {
-    if (req.session.nick) {
-      return res.badRequest("Nickname already set");
+    if (!req.session.uuid) {
+      req.session.uuid = uuid();
+    }
+    if (!req.param('nick')) {
+      return res.badRequest("No nickname provided");
     }
     req.session.nick = req.param('nick');
-    req.session.uid = req.session.nick + "|" + Math.random();
+    req.session.uid = req.session.nick + "|" + req.session.uuid;
     return res.ok();
   }
 
