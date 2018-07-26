b9a63f46bd36acb1e9afdb4ba27aebf02f216799
Eric Zhang <ekzhang1@gmail.com>
2018-07-26 08:31:52 -0700

    Misc improvements

--- api/controllers/RoomController.js
+++ api/controllers/RoomController.js
@@ -21,7 +21,10 @@ module.exports = {
     const room_id = req.param('room_id');
     const events = await Event.find({ room_id: room_id, type: 'created' });
     if (events.length !== 0)
-      return res.view('pages/room.ejs', { room: escapeStr(room_id) });
+      return res.view('pages/room.ejs', {
+        room: escapeStr(room_id),
+        uid: escapeStr(req.session.uid)
+      });
     return res.redirect('/createRoom/' + room_id);
   },
 
@@ -35,13 +38,16 @@ module.exports = {
     if (!host)
       return res.badRequest("No user host");
     const question = req.param('question');
-    const value = req.param('value');
+    const value = Number(req.param('value'));
     const options = req.param('options');
     const room_id = req.param('room_id');
 
-    if (room_id == undefined || room_id.length === 0) {
+    if (room_id == undefined || room_id.length === 0)
       return res.badRequest("Invalid room ID");
-    }
+    if (!question)
+      return res.badRequest("No question provided");
+    if (isNaN(value) || !Number.isInteger(value) || value <= 0)
+      return res.badRequest("Invalid asset value");
 
     const events = await Event.find({ room_id: room_id, type: 'created' });
     if (events.length !== 0) {
