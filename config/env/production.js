4aff6f60752ea3cb68bbe95ece3b5d9689dfe6ee
Eric Zhang <ekzhang1@gmail.com>
2018-12-29 22:00:17 -0600

    Change datastore back to redis

--- config/env/production.js
+++ config/env/production.js
@@ -184,8 +184,8 @@ module.exports = {
     * > (For a full list, see https://sailsjs.com/plugins/sessions)            *
     *                                                                          *
     ***************************************************************************/
-    adapter: 'connect-mongo',
-    url: process.env.MONGODB_URI,
+    adapter: '@sailshq/connect-redis',
+    url: process.env.REDIS_URL,
     //--------------------------------------------------------------------------
     // /\   OR, to avoid checking it in to version control, you might opt to
     // ||   set sensitive credentials like this using an environment variable.
