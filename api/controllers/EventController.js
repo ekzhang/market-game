9a23f61c02cb44c04b73cb53efe503cda93f19e7
Eric Zhang <ekzhang1@gmail.com>
2018-12-29 21:53:28 -0600

    Fix race condition

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -5,6 +5,9 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+var mutexify = require('mutexify');
+var lock = mutexify();
+
 function getRoom(room_id) {
   return Event.find({
     where: { room_id: room_id },
@@ -56,47 +59,63 @@ module.exports = {
   action: async function(req, res) {
     const verb = req.param('verb');
     const room_id = req.param('room_id');
-    const room = await getRoom(room_id);
-    const info = aggregate(room);
-    if (info === null || room.length === 0) {
-      return res.badRequest("No such active room");
-    }
 
-    const user = req.session.uid;
-    let event = {
-      room_id,
-      type: verb,
-      user
-    };
+    lock(async function(release) {
+      const room = await getRoom(room_id);
+      const info = aggregate(room);
+      if (info === null || room.length === 0) {
+        release();
+        return res.badRequest("No such active room");
+      }
 
-    // Verb is: bid/at, taken/sold, end
-    if (verb === 'end') {
-      // End the game
-      if (info.host !== user)
-        return res.forbidden("Not allowed to end this game");
-    }
-    else if (verb === 'bid' || verb === 'at') {
-      const amt = Number(req.param('amt'));
-      if (isNaN(amt) || !Number.isInteger(amt) || amt <= 0 || amt > 1000)
-        return res.badRequest("Invalid number provided");
-      if ((verb === 'bid' && (amt <= info.buy || amt >= info.sell))
-          || (verb === 'at' && (amt >= info.sell || amt <= info.buy)))
-        return res.badRequest("Invalid bid/at cost");
-      event.data = amt;
-    }
-    else if (verb === 'taken' || verb === 'sold') {
-      if ((verb === 'taken' && isNaN(info.sell)) || (verb === 'sold' && isNaN(info.buy)))
-        return res.badRequest("No current offer to take/sell");
-      if ((verb === 'sold' && user === info.buser) || (verb === 'taken' && user === info.suser))
-        return res.badRequest("Cannot take own offer");
-    }
-    else {
-      return res.badRequest("Invalid verb");
-    }
+      const user = req.session.uid;
+      let event = {
+        room_id,
+        type: verb,
+        user
+      };
 
-    await Event.create(event);
-    sails.sockets.broadcast("room" + room_id, "message", event);
-    return res.ok();
+      // Verb is: bid/at, taken/sold, end
+      if (verb === 'end') {
+        // End the game
+        if (info.host !== user) {
+          release();
+          return res.forbidden("Not allowed to end this game");
+        }
+      }
+      else if (verb === 'bid' || verb === 'at') {
+        const amt = Number(req.param('amt'));
+        if (isNaN(amt) || !Number.isInteger(amt) || amt <= 0 || amt > 1000) {
+          release();
+          return res.badRequest("Invalid number provided");
+        }
+        if ((verb === 'bid' && (amt <= info.buy || amt >= info.sell))
+          || (verb === 'at' && (amt >= info.sell || amt <= info.buy))) {
+          release();
+          return res.badRequest("Invalid bid/at cost");
+        }
+        event.data = amt;
+      }
+      else if (verb === 'taken' || verb === 'sold') {
+        if ((verb === 'taken' && isNaN(info.sell)) || (verb === 'sold' && isNaN(info.buy))) {
+          release();
+          return res.badRequest("No current offer to take/sell");
+        }
+        if ((verb === 'sold' && user === info.buser) || (verb === 'taken' && user === info.suser)) {
+          release();
+          return res.badRequest("Cannot take own offer");
+        }
+      }
+      else {
+        release();
+        return res.badRequest("Invalid verb");
+      }
+
+      await Event.create(event);
+      release();
+      sails.sockets.broadcast("room" + room_id, "message", event);
+      return res.ok();
+    });
   }
 
 };
